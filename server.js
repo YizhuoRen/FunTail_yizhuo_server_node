cfa69228ad058d4db80fa041397cb800a067dfc6
Yizhuo Ren <ren.yiz@husky.neu.edu>
2021-04-28 01:34:52 -0700

    project

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://24.22.242.140:3000/', 'https://wbdv-project-yz-client.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', 'https://wbdv-project-yz-client.herokuapp.com');
   res.header('Access-Control-Allow-Headers',
       'Content-Type, X-Requested-With, Origin');
   res.header('Access-Control-Allow-Methods',
