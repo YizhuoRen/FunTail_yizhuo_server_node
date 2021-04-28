709f2ec3f6464db211de21684b273ce7d43e0ffe
Yizhuo Ren <ren.yiz@husky.neu.edu>
2021-04-28 12:48:56 -0700

    project

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://wbdv-project-yz-client.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers',
       'Content-Type, X-Requested-With, Origin');
   res.header('Access-Control-Allow-Methods',
@@ -27,6 +27,8 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
+
+// 'https://wbdv-project-yz-client.herokuapp.com'
 
 app.use(bodyParser.urlencoded({
   extended: true
