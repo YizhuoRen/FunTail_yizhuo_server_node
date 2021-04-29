0048cd25c0a7ea4dde41de6579f27bf0de21a2df
Yizhuo Ren <ren.yiz@husky.neu.edu>
2021-04-28 21:57:14 -0700

    project

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ require('dotenv').config()
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).then(res => console.log("Connected to DB"))
 .catch(err => console.log(err))
+const cors = require("cors");
+app.use(cors({credentials: true, origin: "https://wbdv-project-yz-client.herokuapp.com"}));
 
 const session = require('express-session')
 app.use(session({
@@ -18,15 +20,16 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers',
-      'Content-Type, X-Requested-With, Origin');
-  res.header('Access-Control-Allow-Methods',
-      'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers',
+//       'Content-Type, X-Requested-With, Origin');
+//   res.header('Access-Control-Allow-Methods',
+//       'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header('Content-Type', 'application/json; charset=utf-8');
+//   next();
+// });
 
 // 'https://wbdv-project-yz-client.herokuapp.com'
 
